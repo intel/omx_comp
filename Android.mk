c71a52033b2f66133698394e7a94ff599d69b59e
Daniel Charles <daniel.charles@intel.com>
2012-03-09 13:58:56 -0800

    Remove unused legacy code
    These components are no longer used on the medfield multimedia solution so
    they are removed.
    
    Change-Id: I450fef8590ad9fc82839d42fa1d11c0ca912c67f
    Signed-off-by: Daniel Charles <daniel.charles@intel.com>

--- Android.mk
+++ Android.mk
@@ -20,10 +20,6 @@ LIBVA_TOP := hardware/intel/libva
 #-include $(VENDORS_INTEL_MRST_COMPONENTS_ROOT)/sst/Android.mk
 
 #intel video codecs
-include $(VENDORS_INTEL_MRST_COMPONENTS_ROOT)/psb-dec/Android.mk
-#include $(VENDORS_INTEL_MRST_COMPONENTS_ROOT)/avc-enc/Android.mk
-#include $(VENDORS_INTEL_MRST_COMPONENTS_ROOT)/h263-enc/Android.mk
-#include $(VENDORS_INTEL_MRST_COMPONENTS_ROOT)/m4v-enc/Android.mk
 include $(VENDORS_INTEL_MRST_COMPONENTS_ROOT)/videocodec/Android.mk
 
 #intel audio codecs
