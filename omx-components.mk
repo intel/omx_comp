11a5ef93616166f4844a07d8e496c6d98797ad49
Haitao Huang <haitao.huang@intel.com>
2012-07-06 12:15:25 -0500

    Add hw encoders
    Change-Id: I995a1de101850ef890fa1ae281f74157129f0ec0
    Signed-off-by: Haitao Huang <haitao.huang@intel.com>

--- omx-components.mk
+++ omx-components.mk
@@ -3,4 +3,5 @@ USE_INTEL_OMX_COMPONENTS := true
 PRODUCT_PACKAGES += \
 	wrs_omxil_components.list \
 	libOMXVideoDecoderAVC	  \
-	libOMXVideoDecoderWMV
+	libOMXVideoDecoderWMV     \
+        libOMXVideoEncoderAVC
