quanganh2627/baytrail-i-x64-kitkat_vendor_intel_hardware_omx-components

VEAVC: Add return vaule check for VideoEncoderAVC::getOutput
BZ: 28122

Add return vaule check for VideoEncoderAVC::getOutput

Change-Id: I10894467ef07458c085406f7407a3467d246b159
Signed-off-by: Chang Ying <ying.chang@intel.com>
Reviewed-on: http://android.intel.com:8080/46666
Reviewed-by: Yuan, Shengquan <shengquan.yuan@intel.com>
Reviewed-by: Ding, Haitao <haitao.ding@intel.com>
Tested-by: Ding, Haitao <haitao.ding@intel.com>
Reviewed-by: buildbot <buildbot@intel.com>
Tested-by: buildbot <buildbot@intel.com>

videocodec/OMXVideoEncoderAVC.cpp
@@ -301,7 +301,7 @@ OMX_ERRORTYPE OMXVideoEncoderAVC::ProcessorProcess(
                 mFirstFrame = OMX_FALSE;
             } else {
                 outBuf.format = OUTPUT_EVERYTHING;
-                mVideoEncoder->getOutput(&outBuf);
+                ret = mVideoEncoder->getOutput(&outBuf);
                 CHECK_ENCODE_STATUS("getOutput");
 
                 LOGV("output data size = %d", outBuf.dataSize);
@@ -355,7 +355,7 @@ OMX_ERRORTYPE OMXVideoEncoderAVC::ProcessorProcess(
                 mFirstFrame = OMX_FALSE;
             } else {
                 outBuf.format = OUTPUT_LENGTH_PREFIXED;
-                mVideoEncoder->getOutput(&outBuf);
+                ret = mVideoEncoder->getOutput(&outBuf);
                 CHECK_ENCODE_STATUS("getOutput");
 
                 LOGV("output data size = %d", outBuf.dataSize);

intel/omx_comp

Signed-off-by: Haitao Huang <haitao.huang@intel.com>

videocodec/OMXVideoEncoderAVC.cpp
@@ -164,7 +164,7 @@ OMX_ERRORTYPE OMXVideoEncoderAVC::ProcessorProcess(
 
             outBuf.format = OUTPUT_EVERYTHING;
             ret = mEncoderVideo->getOutput(&outBuf);
-            CHECK_ENCODE_STATUS("encode");
+            CHECK_ENCODE_STATUS("getOutput");
 
             LOGV("output data size = %d", outBuf.dataSize);
             outfilledlen = outBuf.dataSize;
@@ -252,7 +252,7 @@ OMX_ERRORTYPE OMXVideoEncoderAVC::ProcessorProcess(
                 mFirstFrame = OMX_FALSE;
             } else {
                 outBuf.format = OUTPUT_EVERYTHING;
-                mEncoderVideo->getOutput(&outBuf);
+                ret = mEncoderVideo->getOutput(&outBuf);
                 CHECK_ENCODE_STATUS("getOutput");
 
                 LOGV("output data size = %d", outBuf.dataSize);