b3f10d96e4b00a54cc997992f5885c335c0ed073
Daniel Charles <daniel.charles@intel.com>
2012-08-17 15:42:43 -0700

    omx-components:  remove unsupported components from compilation
    The decoders depend on libraries that are not compiled anymore, so
    they should go away.
    
    Change-Id: Icb080bb31abf0c3591e536a764c54fa8f38f34b9
    Depends-Change-Id: If0ed62fa44d462c5790df27a8e1716671483991b
    Depends-Change-Id: Ie6ee1d9a8b58933b5d7ba89dd91b9caa3f8873c0
    Signed-off-by: Daniel Charles <daniel.charles@intel.com>

--- videocodec/Android.mk
+++ videocodec/Android.mk
@@ -39,69 +39,6 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := libOMXVideoDecoderAVC
 include $(BUILD_SHARED_LIBRARY)
 
-
-include $(CLEAR_VARS)
-
-LOCAL_CFLAGS := $(OMXLOG_CFLAGS) -DLOG_TAG=\"omx-mpeg4dec\"
-LOCAL_CPPFLAGS :=
-LOCAL_LDFLAGS :=
-
-LOCAL_SHARED_LIBRARIES := \
-    libwrs_omxil_common \
-    libva_videodecoder \
-    libmixvbp_mpeg4 \
-    liblog \
-    libva \
-    libva-android \
-
-LOCAL_C_INCLUDES := \
-    $(WRS_OMXIL_CORE_ROOT)/utils/inc \
-    $(WRS_OMXIL_CORE_ROOT)/base/inc \
-    $(WRS_OMXIL_CORE_ROOT)/core/inc/khronos/openmax/include \
-    $(PV_INCLUDES) \
-    $(TARGET_OUT_HEADERS)/libmixcodec \
-    $(TARGET_OUT_HEADERS)/libva
-
-LOCAL_SRC_FILES := \
-    OMXComponentCodecBase.cpp\
-    OMXVideoDecoderBase.cpp\
-    OMXVideoDecoderMPEG4.cpp
-
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE := libOMXVideoDecoderMPEG4
-include $(BUILD_SHARED_LIBRARY)
-
-include $(CLEAR_VARS)
-
-LOCAL_CFLAGS := $(OMXLOG_CFLAGS) -DLOG_TAG=\"omx-h263dec\"
-LOCAL_CPPFLAGS :=
-LOCAL_LDFLAGS :=
-
-LOCAL_SHARED_LIBRARIES := \
-    libwrs_omxil_common \
-    libva_videodecoder \
-    liblog \
-    libva \
-    libva-android
-
-LOCAL_C_INCLUDES := \
-    $(WRS_OMXIL_CORE_ROOT)/utils/inc \
-    $(WRS_OMXIL_CORE_ROOT)/base/inc \
-    $(WRS_OMXIL_CORE_ROOT)/core/inc/khronos/openmax/include \
-    $(PV_INCLUDES) \
-    $(TARGET_OUT_HEADERS)/libmixcodec \
-    $(TARGET_OUT_HEADERS)/libva
-
-LOCAL_SRC_FILES := \
-    OMXComponentCodecBase.cpp\
-    OMXVideoDecoderBase.cpp\
-    OMXVideoDecoderH263.cpp
-
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE := libOMXVideoDecoderH263
-include $(BUILD_SHARED_LIBRARY)
-
-
 include $(CLEAR_VARS)
 
 LOCAL_CFLAGS := $(OMXLOG_CFLAGS) -DLOG_TAG=\"omx-wmvdec\"
